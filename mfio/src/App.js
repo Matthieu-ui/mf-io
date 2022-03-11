11c2649313cbb9fd360990034fa2ac2a0acc7505
matthieu-ui <mattfelker206@outlook.com>
2022-03-10 21:10:53 -0800

    create card

---
+++ mfio/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../src/css/skeleton.css';
+function App() {
+  return (
+
+
+    <div className="App">
+
+      <body>
+        <div className="container">
+          <div className='layer1'>
+          <div className="row">
+            <div className="four columns">
+              <div className='mfio-card'>
+                <img className='avi' src='https://avatars.githubusercontent.com/u/77665167?v=4'></img>
+                <div className='name-plate'>
+                <h4><b>Matthieu</b> Felker</h4>
+                </div>
+                <div className='title'>
+                <p>Web Developer and Graphic Artist</p>
+                </div>
+            
+              </div>
+            </div>  
+          </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
